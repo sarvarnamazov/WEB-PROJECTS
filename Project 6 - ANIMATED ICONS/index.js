0fc71c04c0154938438bee1f61dd03011701843a
Sarvarjon Namazov <87962978+sarvarnamazov@users.noreply.github.com>
2022-06-24 21:25:13 +0500

    Add files via upload

---
+++ Project 6 - ANIMATED ICONS/index.js
@@ -0,0 +1,10 @@
+let menu = document.querySelector('#menu')
+let menu2 = document.querySelector('#menu2')
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('openmenu')
+})
+
+menu2.addEventListener('click', () => {
+    menu2.classList.toggle('openmenu2')
+})
